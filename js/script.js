e75578af9fa4a21bd45a4b647d87bca247039f46
MatiasBartoluche <bartoluche.matias@gmail.com>
2025-07-11 14:00:04 -0300

    Aplicando estilos css y animaciones al header

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
+
     animatedHeader();
 });
 
@@ -12,13 +12,31 @@ function animatedHeader(){
         let scrollTop = window.scrollY || document.documentElement.scrollTop;
 
         if(scrollTop === 0){
-            console.log('removiendo clase');
             header.classList.remove('animated-header');
+            scrollController();
         }
         else if(scrollTop > lastScrollTop){
-            console.log('agregando clase');
             header.classList.add('animated-header');
         }
         lastScrollTop = scrollTop;
     });
+}
+
+// controla que el primer scroll mueva solo 1px, luego se desplaza con normalidad
+// esto evita que el #main-container se muestre inicialmente por debajo del nav
+function scrollController(){
+    //let hasDoneInitialScroll = false; // controla si se ha hecho scroll por primera vez
+    window.addEventListener("wheel", function (e) {
+        const atTop = window.scrollY === 0; // scroll = 0 para no scroll o para volver arriba de la pagina
+        const scrollingDown = e.deltaY > 0; // controla la altura del scroll
+
+        if (atTop && scrollingDown) {
+            e.preventDefault(); // evitar el scroll natural
+            window.scrollTo({
+                top: 1,
+                behavior: "smooth"
+            });
+            //hasDoneInitialScroll = true;
+        }
+    }, { passive: false });
 }
