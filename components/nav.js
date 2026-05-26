447b66c76435a10b6bc7f4209bfa344b0447a816
MatiasBartoluche <bartoluche.matias@gmail.com>
2026-05-25 21:29:41 -0300

    Modificando el codigo de nav.js para que pueda construir menus de forma dinamica segun la pagina que lo utilice

--- components/nav.js
+++ components/nav.js
@@ -1,16 +1,31 @@
 class NavComponent extends HTMLElement{
     connectedCallback(){
-        this.innerHTML =`
-            <ul>
-                <!-- cada link hace un desplazamiento interno a su seccion correspondiente -->
-                <li> <a href="#first-section" id="option1">Sobre mí</a> </li>
-                <li> <a href="#second-section">Formación académica</a> </li>
-                <li> <a href="#thirth-section">Tecnologías</a> </li>
-                <li> <a href="#fourth-section">Habilidades</a> </li>
-                <li> <a href="#fifth-section">Proyectos</a> </li>
-                <li> <a href="#comments-section">Comentarios</a> </li>
-            </ul>
-        `;
+        const page = this.getAttribute('page');
+        var construir_menu ='';
+        var menu ='';
+
+        // detectar de que pagina se usara el componente
+        if(page === 'index'){
+            menu = [
+                {text:'Sobre mí', href:'#first-section'},
+                {text:'Formación académica', href:'#first-section'},
+                {text:'Tecnologías', href:'#second-section'},
+                {text:'Habilidades', href:'#thirth-section'},
+                {text:'Proyectos', href:'#fourth-section'},
+                {text:'Comentarios', href:'#fifth-section'}
+            ]
+        }
+
+        // convierto el objeto a texto para insertarlo en la pagina
+        menu.forEach(item => {
+            //console.log(`${item.text} - ${item.href}`);
+            construir_menu = construir_menu + '<li>'+
+                                                    '<a href="'+item.href+'"> '+item.text+' </a>'+
+                                                '</li>'
+        });
+
+        // inserto el texto en la pagina
+        this.innerHTML ='<ul>'+construir_menu+'</ul>';
     }
 }
 
