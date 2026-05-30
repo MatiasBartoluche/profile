098b7cceadf75e76a2e4f46bb7eb3f0913f5ba19
MatiasBartoluche <bartoluche.matias@gmail.com>
2026-05-30 13:44:51 -0300

    Creando el component proyect_card.js e insertando proyectos con el nuevo component

--- js/script.js
+++ js/script.js
@@ -15,24 +15,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
     //newComment();
     //loadComments();
-
-    var section1 = document.getElementById('first-section');
-    var section2 = document.getElementById('second-section');
-    var section3 = document.getElementById('thirth-section');
-    var section4 = document.getElementById('fourth-section');
-    var section5 = document.getElementById('fifth-section');
-
-    // insertando articulos en la seccion de proyectos
-    var links_presentacion = {info: './pages/warning.html', page: 'https://matiasbartoluche.github.io/profile/', github: 'https://github.com/MatiasBartoluche/profile'};
-    var keyworlds_presentacion = '<p class="keys">Keyworlds: </p>'+'<p class="html">HTML</p>'+'<p class="css">CSS</p>'+'<p class="javascript">JavaScript</p>';
-    var info_presentacion = "Página en la que enlisto mis habilidades, conocimientos de tecnologías y proyectos y repositorios en los que he estado trabajando. El aspecto de la paginacambiará periodicamente para hacerla mas atractiva y estetica.";
-    addProyectArticle(section5, './img/default-proyect.jpg', 'Página de presentación', info_presentacion, links_presentacion, keyworlds_presentacion);
-
-    var links_fases_luna ={info:'./pages/warning.html', page:'https://matiasbartoluche.github.io/realidad-aumentada-jardin/', github:'https://github.com/MatiasBartoluche/realidad-aumentada-jardin'};
-    var keyworlds_fases_luna = '<p class="keys">Keyworlds: </p>'+'<p class="html">HTML</p>'+'<p class="html">A-Frame</p>'+'<p class="css">CSS</p>'+'<p class="css">Bootstrap</p>'+'<p class="javascript">JavaScript</p>'+'<p class="javascript">AR-JS</p>'+'<p class="css">Respoinsive desing</p>';
-    var info_fases_luna = 'Aplicacion web creada con el objetivo de ilustrar las fases de la luna de forma didactica. A través de dispositivos moviles, escanea imagenes determinadas y visualiza modelos 3D del sol, la tierra y la luna. Pensado para alumnos de nivel inicial.';
-    addProyectArticle(section5, './img/fases-luna.png', 'Fases de la luna con realidad aumentada', info_fases_luna, links_fases_luna, keyworlds_fases_luna);
-
   });
 
 // funcion que captura datos del formulario y lo inserta en la pagina
@@ -162,57 +144,4 @@ async function loadComments() {
                                         "</div>";
         });
       }
-}
-
-// funcion que inserta articulos de soft skills
-// container = contenedor donde se va a insertar el articulo
-// title = String - tilulo del articulo
-// image = String - ruta del archivo de imagen (debe introducirse en formato texto)
-// info = String - informacion del articulo
-function addSkillArticle(container, title, image, info){
-  var contenedor = container;
-
-  contenedor.innerHTML += "<article class='card-article glow-circle glow-green'>"+
-                            "<h2>"+title+"</h2>"+
-                            "<div class='info-container'>"+
-                                "<img src='"+image+"'>"+
-                                "<p>"+info+"</p>"+
-                            "</div>"+
-                          "</article>"
-}
-
-// funcion que inserta articulos de proyectos
-// container = contenedor donde se insertaran los articulos
-// image = String - ruta del archivo de imagen
-// title = String - titulo del articulo
-// description = String - descripcion del articulo
-// links = object - objeto que consta de tres atributos string, links para botones
-function addProyectArticle(container, image, title, description, links, keyworlds){
-  var contenedor = container;
-
-  contenedor.innerHTML += "<article class='card-proyect glow-circle glow-rainbow'>"+
-                              "<div class='img-content'>"+
-                                  "<img src='"+image+"'>"+
-                              "</div>"+
-                              "<h2>"+title+"</h2>"+
-                              "<p>"+description+"</p>"+
-                              "<div class='keyworlds'>"+keyworlds+"</div>"+
-                              
-                              "<div class='proyect-buttons'>"+
-                                  "<a class='custom-button light-button' href='"+links.info+"'>"+
-                                      "<img src='./img/details-white.png'>"+
-                                      "<p>Details</p>"+
-                                  "</a>"+
-                        
-                                  "<a class='custom-button light-button' href='"+links.page+"'>"+
-                                      "<img src='./img/internet-white.png'>"+
-                                      "<p>Visit page</p>"+
-                                  "</a>"+
-                                  
-                                  "<a class='custom-button light-button' href='"+links.github+"'>"+
-                                      "<img src='./img/github.png'>"+
-                                      "<p>Github</p>"+
-                                  "</a>"+
-                              "</div>"+
-                          "</article>";
 }
